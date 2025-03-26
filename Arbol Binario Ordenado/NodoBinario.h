1db67eda2cfcb585571f8553641d5f67378978bd
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-26 12:30:02 -0500

    Update NodoBinario.h

--- Arbol Binario Ordenado/NodoBinario.h
+++ Arbol Binario Ordenado/NodoBinario.h
@@ -1,23 +1,73 @@
+/*******************************************
+Fecha: 26 marzo 2025
+Autor: Santiago Camargo
+Tema: Taller Árboles
+Estructuras de datos
+******************************************/
 #ifndef NODOBINARIO_H
 #define NODOBINARIO_H
 
+/**
+ * @file NodoBinario.h
+ * @brief Definición de la clase NodoBinario.
+ *
+ * Clase que representa un nodo de un árbol binario, con un dato y punteros a sus hijos izquierdo y derecho.
+ */
+
 template <class T>
+class NodoBinario {
+protected:
+    T dato;                      ///< Dato almacenado en el nodo.
+    NodoBinario<T> *hijoIzq;     ///< Puntero al hijo izquierdo.
+    NodoBinario<T> *hijoDer;     ///< Puntero al hijo derecho.
 
-class NodoBinario{
-	protected:
-		T dato;
-		NodoBinario<T> *hijoIzq;
-		NodoBinario<t> *hijoDer;
+public:
+    /**
+     * @brief Constructor de la clase NodoBinario.
+     * Inicializa los punteros de los hijos en NULL.
+     */
+    NodoBinario();
 
-	public:
-		NodoBinario();
-		~NodoBinario();
-		T& getDato();
-		void setDato(T& val);
-		NodoBinario<T>* getHijoIzq();
-		NodoBinario<T>* getHijoDer();
-		void setHijoIzq(NodoBinario<T> *izq);
-		void setHijoDer(NodoBinario<T> *der);
+    /**
+     * @brief Destructor de la clase NodoBinario.
+     */
+    ~NodoBinario();
+
+    /**
+     * @brief Obtiene el dato almacenado en el nodo.
+     * @return Referencia al dato del nodo.
+     */
+    T& getDato();
+
+    /**
+     * @brief Establece el dato del nodo.
+     * @param val Valor a asignar al nodo.
+     */
+    void setDato(T& val);
+
+    /**
+     * @brief Obtiene el puntero al hijo izquierdo.
+     * @return Puntero al nodo hijo izquierdo.
+     */
+    NodoBinario<T>* getHijoIzq();
+
+    /**
+     * @brief Obtiene el puntero al hijo derecho.
+     * @return Puntero al nodo hijo derecho.
+     */
+    NodoBinario<T>* getHijoDer();
+
+    /**
+     * @brief Establece el hijo izquierdo del nodo.
+     * @param izq Puntero al nuevo hijo izquierdo.
+     */
+    void setHijoIzq(NodoBinario<T> *izq);
+
+    /**
+     * @brief Establece el hijo derecho del nodo.
+     * @param der Puntero al nuevo hijo derecho.
+     */
+    void setHijoDer(NodoBinario<T> *der);
 };
 
-#endif //NODOBINARIO_H
+#endif // NODOBINARIO_H
